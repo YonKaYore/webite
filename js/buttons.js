10303f5496c08a60739cd261d523c08c11f5e80d
YonKaYore <yonkagorreal@gmail.com>
2026-02-28 00:42:56 +0100

    Added dark theme switch, fixed styling on IE5

--- js/buttons.js
+++ js/buttons.js
@@ -7,3 +7,30 @@ function changeStyle() {
         hrs[i].style.color = '#ffffff';
     }
 }
+
+var darkMode = false;
+
+function toggleTheme() {
+    var hrs = document.getElementsByTagName('hr');
+    var i;
+
+    if (darkMode == false) {
+        document.body.style.backgroundColor = '#373841';
+        document.body.style.color = '#ffffff';
+        
+        for (i = 0; i < hrs.length; i++) {
+            hrs[i].style.color = '#ffffff';
+        }
+        
+        darkMode = true;
+    } else {
+        document.body.style.backgroundColor = '#ffffff';
+        document.body.style.color = '#000000';
+        
+        for (i = 0; i < hrs.length; i++) {
+            hrs[i].style.color = '#000000';
+        }
+        
+        darkMode = false;
+    }
+}
